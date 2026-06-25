6307ac7ffadabaf7763afa7c61da8c6f94681a0f
Svetozavrik <lana.svet.bazhenova@mail.ru>
2026-06-25 14:54:07 +0700

    изменения

--- test_lesson06_task1.py
+++ test_lesson06_task1.py
@@ -6,16 +6,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_dynamic_loading():
     driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
-    start_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "start"))
-    )
-    start_button.click()
-    wait = WebDriverWait(driver, 10)
-    hello_world_element = wait.until(
-            EC.visibility_of_element_located((By.ID, "Hello Word!"))
+    try:
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
+
+        start_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "start"))
         )
-    driver.save_screenshot("result.png")
-    assert hello_world_element.text == "Hello World!"
-    
-    driver.quit()
+        start_button.click()
+
+        wait = WebDriverWait(driver, 10)
+        hello_world_element = wait.until(
+            EC.visibility_of_element_located((By.ID, "finish"))
+        )
+
+        driver.save_screenshot("result.png")
+        assert hello_world_element.text == "Hello World!"
+    finally:
+        driver.quit()
